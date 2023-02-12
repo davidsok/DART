ebf5502ced0715390b226cd322ccd2fbd6178314
David Sok <davidsok7@yahoo.com>
2023-02-12 14:29:53 -0800

    add more basics

--- list.dart
+++ list.dart
@@ -17,3 +17,25 @@ void main() {
   print(list1.indexOf(33));
   print(list1.length);
 }
+// Mutable List
+List<String> names = ["Raj", "John", "Rocky"]; 
+names[1] = "Bill"; // possible
+names[2] = "Elon"; // possible
+    
+// Immutable List
+const List<String> names = ["Raj", "John", "Rocky"]; 
+names[1] = "Bill"; // not possible
+names[2] = "Elon"; // not possible
+
+/*
+
+List Properties In Dart
+first: It returns the first element in the List.
+last: It returns the last element in the List.
+isEmpty: It returns true if the List is empty and false if the List is not empty.
+isNotEmpty: It returns true if the List is not empty and false if the List is empty.
+length: It returns the length of the List.
+reversed: It returns a List in reverse order.
+single: It is used to check if the List has only one element and returns it.
+
+*/
